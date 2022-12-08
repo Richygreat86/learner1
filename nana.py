2542a2c0c262f09b793294165712a197e0599433
richygreat <richardomizegba@gmail.com>
2022-12-08 05:01:08 -0700

    another line of code

--- nana.py
+++ nana.py
@@ -1,3 +1,8 @@
 print("hello", end="! ");\
             print("python is a great programing language")
 hello! python is a great programing language
+
+
+friends = ("jim", "karen", "Kevin")
+for friend in friends:
+        print (friend)
