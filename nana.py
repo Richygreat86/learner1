27e59d62a6592ea5013ec73babadfb901c559190
Richygreat86 <118113868+Richygreat86@users.noreply.github.com>
2022-12-08 05:07:43 -0700

    Update nana.py
    New code

--- nana.py
+++ nana.py
@@ -1,3 +1,6 @@
 print("hello", end="! ");\
             print("python is a great programing language")
 hello! python is a great programing language
+friends = ("jim", "karen", "Kevin")
+for friend in friends:
+    print (friend)
